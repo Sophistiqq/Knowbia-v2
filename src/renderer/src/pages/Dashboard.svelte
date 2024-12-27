7ebec2e66966ceee2ed8140ab00209855bb0472c
Roi Borromeo <roi.for.school@gmail.com>
2024-12-27 10:47:26 +0000

    Added Line Chart on Dashboard

---
+++ src/renderer/src/pages/Dashboard.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { Line } from 'svelte-chartjs'
+  import { data, options } from '../utils/chart-data'
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale
+  } from 'chart.js'
+
+  ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale)
+</script>
+
+<div class="container">
+  <h2>Dashboard</h2>
+  <div class="line-chart">
+    <Line {data} {options} />
+  </div>
+</div>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    margin: 0 auto;
+    padding-right: 15px;
+    h2 {
+      margin-block: 1rem;
+    }
+  }
+  .line-chart {
+    height: max-content;
+    width: max-content;
+    padding: 1rem;
+    border: var(--border);
+    background-color: var(--background);
+  }
+</style>
+
---
+++ src/renderer/src/utils/chart-data.js
@@ -0,0 +1,43 @@
+
+export const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255, 99, 132, 1)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: 'My Second dataset',
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1,
+      data: [28, 48, 40, 19, 86, 27, 90]
+    }
+  ]
+}
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      labels: {
+        color: 'white'
+      }
+    }
+  },
+  scales: {
+    x: {
+      ticks: {
+        color: 'white'
+      }
+    },
+    y: {
+      ticks: {
+        color: 'white'
+      }
+    }
+  }
+}
